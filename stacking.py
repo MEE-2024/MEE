ecc372896232c3ac40ced5ce54dd1ff6460274e1
Gourlie <gourlik@linnbenton.edu>
2022-09-06 14:24:33 -0700

    it works. No more direct paths

--- stacking.py
+++ stacking.py
@@ -6,18 +6,18 @@ from astropy.io import fits
 import os
 n = 0 
 fitlist = []
-pathofdir = 'fits files'
+pathofdir = 'Mount-Control\\fits files'
 for filename in os.listdir(pathofdir):
     f = os.path.join(pathofdir, filename)
     fitlist.append(fits.getdata(f))
 
 finalimage = np.sum(fitlist, axis = 0)
-outfile = "fitsimage.fts"
+outfile = "Mount-Control\\fitsimage.fts"
 hdu = fits.PrimaryHDU(finalimage)
 hdu.writeto(outfile, overwrite = True)
 
 
-imagedata = fits.getdata("fitsimage.fts")
+imagedata = fits.getdata("Mount-Control\\fitsimage.fts")
 plt.imshow(imagedata, cmap = 'gray', vmin = 2400, vmax = 2700)
 plt.show()
 
