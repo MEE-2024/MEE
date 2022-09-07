cb5a437e0ec0286d1bac2febfb158e7b0ae16f66
Gourlie <gourlik@linnbenton.edu>
2022-09-07 13:41:30 -0700

    h

---
+++ fitsimageviewer.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Set up matplotlib
+import matplotlib.pyplot as plt
+from astropy.io import fits
+path = input("Please paste the direct path of the chosen fits file to view: ")
+imagedata = fits.getdata(path)
+
+print(type(imagedata))    #numpy.ndarray
+print(imagedata.shape)    #(3520, 4656)
+
+
+
+
+
+image = plt.imshow(imagedata, cmap='gray')
+plt.show()
+plt.colorbar()
+
+
+
+   
