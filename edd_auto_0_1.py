398b2864999a49cbdfbbd8b9a2863932169ae294
Gourlie <gourlik@linnbenton.edu>
2022-09-08 23:35:32 -0700

    ewrshj

--- edd_auto_0_1.py
+++ edd_auto_0_1.py
@@ -179,6 +179,7 @@ def user_validation_station(validate_choice):
                 greg = False
             else:                                        
                 print("INVALID INPUT")
+            #prompt user input for partution directory(windows, linux, MACos) 
             main_dir = "D:"
             title = file_name                               #Places folder into external flashdrive. Could be issue for Linux or MAC
             dir = os.path.join(main_dir, title)            #create path using D:\ + "filename"
